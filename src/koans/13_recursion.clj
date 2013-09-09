5180af89ae4ec8cb3c68b4e937c1fc7f85515e87
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-09-09 15:05:40 +0200

    13 stages finished

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,31 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (empty? coll)
+      []
+      (conj (recursive-reverse (rest coll)) (first coll))
+    )
+  )
+
+;(defn factorial [n]
+;  (if (<= n 1) 1 (* n (factorial (- n 1)))))
+
 
 (defn factorial [n]
-  __)
+    (loop [n n acc 1]
+        (if (<= n 1) acc (recur (dec n) (* acc n)))
+    )
+  )
 
 (meditations
   "Recursion ends with a base case"
